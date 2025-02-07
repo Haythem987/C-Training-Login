6e13727019fb99362b998b5089abc38b5ff84cfb
Haythem <3030746629@qq.com>
2025-02-07 22:26:21 +0800

    登录系统的完善

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -9,11 +9,29 @@ public class Program
     {
         Console.WriteLine("请输入用户名");
         var username = Console.ReadLine();
+        if (username == "")
+        {
+            Console.WriteLine("输入内容为空，请重新输入");
+            Register();
+        }
+
         Console.WriteLine("请输入密码");
         var password = Console.ReadLine();
+        if (password == "")
+        {
+            Console.WriteLine("输入内容为空，请重新输入");
+            Register();
+        }
+
         Console.WriteLine("请输入邮箱");
         var email = Console.ReadLine();
-        if (User.Any(x => x.Username == username))
+        if (email == "")
+        {
+            Console.WriteLine("输入内容为空，请重新输入");
+            Register();
+        }
+
+        if (User.Any(x => x.Email == email))
         {
             Console.WriteLine("SB你注册过了");
         }
@@ -26,24 +44,47 @@ public class Program
 
     public static void Login()
     {
-        Console.WriteLine("请输入用户名");
-        var username = Console.ReadLine();
-        Console.WriteLine("请输入密码");
-        var password = Console.ReadLine();
-        if (User.Any(x=>x.Username==username && x.Password==password))
+        if (!User.Any())
         {
-            Console.WriteLine("登陆成功");
-        }
-        else if (User.Any(x => x.Username == username && x.Password != password))
-        {
-            Console.WriteLine("密码错误");
+            Console.WriteLine("未有用户注册过");
         }
         else
         {
-            Console.WriteLine("无此用户");
+            Console.WriteLine("请输入邮箱");
+            var email = Console.ReadLine();
+            if (email == "")
+            {
+                Console.WriteLine("输入内容为空，请重新输入");
+                Login();
+            }
+
+            Console.WriteLine("请输入密码");
+            var password = Console.ReadLine();
+            if (password == "")
+            {
+                Console.WriteLine("输入内容为空，请重新输入");
+                Login();
+            }
+
+            if (User.Any(x => x.Username == email && x.Password == password))
+            {
+                var userinfo = User.FirstOrDefault(x => x.Email == email && x.Password == password);
+                Console.WriteLine($"登陆成功，{userinfo.Username}");
+            }
+            else if (User.Any(x => x.Username == email && x.Password != password))
+            {
+                Console.WriteLine("密码错误");
+            }
+            else if (User.Any(x => x.Email != email))
+            {
+                Console.WriteLine("邮箱错误");
+            }
+            else
+            {
+                Console.WriteLine("无此用户");
+            }
         }
     }
-
 
     public static void Main(string[] args)
     {
@@ -55,27 +96,40 @@ public class Program
             Console.WriteLine("3.查询用户列表");
             Console.WriteLine("4.退出");
             var input = Console.ReadLine();
-            if (input == "1")
+            try
             {
-                Register();
-            }
-            else if (input == "2")
-            {
-                Login();
-            }
-            else if (input == "3")
-            {
-                foreach (var item in User) Console.WriteLine($"用户名:{item.Username},密码:{item.Password},邮箱:{item.Email}");
+                var inputnum = Convert.ToInt32(input);
+                if (inputnum is 0 or > 4)
+                {
+                    Console.WriteLine("请输入在1-4之间的整数");
+                    continue;
+                }
+
+                switch (inputnum)
+                {
+                    case 1:
+                        Register();
+                        break;
+                    case 2:
+                        Login();
+                        break;
+                    case 3:
+                        foreach (var item in User)
+                            Console.WriteLine($"用户名:{item.Username},密码:{item.Password},邮箱:{item.Email}");
+                        break;
+                    case 4:
+                        Console.WriteLine("再见");
+                        Environment.Exit(0);
+                        break;
+                }
             }
 
-
-            else if (input == "4")
+            catch
             {
-                Console.WriteLine("再见");
-                break;
+                Console.WriteLine("你在输毛");
             }
         }
-    }
+        }
 
     public class Useritem
     {
